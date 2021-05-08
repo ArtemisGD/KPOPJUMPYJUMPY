60146b97b9c991e2d57f1cef50c9ecb30f0d3c30
ArtemisGD <danaedango@gmail.com>
2021-05-08 10:45:56 -0500

    Movimiento personaje

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    private Rigidbody2D _rigidbody;
+    public float moveSpeed;
+    public Vector2 input;
+
+    void Start()
+{
+_rigidbody = GetComponent<Rigidbody2D>();
+}
+
+void Update()
+{
+    Vector2 input = new Vector2(Input.GetAxis("Horizontal")*moveSpeed, _rigidbody.velocity.y);
+        _rigidbody.velocity = input;
+    }
+    }
