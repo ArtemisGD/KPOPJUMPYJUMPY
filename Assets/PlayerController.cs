2d7153ff817aa9910b1bbda9eaa83aae0a19157b
ArtemisGD <danaedango@gmail.com>
2021-05-08 11:22:33 -0500

    Saltar y mover

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{ 
+{
     private Rigidbody2D _rigidbody;
     public float moveSpeed;
+    public float jumpForce;
     public Vector2 input;
 
     void Start()
-{
-_rigidbody = GetComponent<Rigidbody2D>();
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, _rigidbody.velocity.y);
+        _rigidbody.velocity = input;
+
+        if (Input.GetButtonDown("Jump"))
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+    }
 }
 
-void Update()
-{
-    Vector2 input = new Vector2(Input.GetAxis("Horizontal")*moveSpeed, _rigidbody.velocity.y);
-        _rigidbody.velocity = input;
-    }
-    }
+
+		  
